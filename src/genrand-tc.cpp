711b084bd362650351315516b73c2e3dcb90ef88
Antonio Gemesson <antonio.gemesson@ufpi.edu.br>
2025-12-28 15:43:57 -0300

    Improves the random number generator function

--- src/genrand-tc.cpp
+++ src/genrand-tc.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <sstream>
 #include <utility>
 
 // This program can generate random values for stress testing problems solutions 
 
+const unsigned int P = 4294967291U;
+const unsigned int multi = 16807U;
 // This function Generate random values in a certain range
 long long gen_rand_in_range(long long a, long long b){
     if(a > b) std::swap(a, b);
@@ -12,14 +15,22 @@ long long gen_rand_in_range(long long a, long long b){
     return value;
 }
 
-/* argv[1] needs to be the seed for generate values
- * argv[2] needs to be the number of test cases
- * argv[3] needs to be the inferior and superior limit of the range
- * argv[4] needs to be the amount of numbers that needs to be generated for each test case
- *
- * */
+unsigned int gen_rand_seed(unsigned int n){
+    time_t seconds = time(NULL);
+    unsigned long long temp = ((unsigned long long)seconds + (unsigned long long)clock()) * multi + n;
+    n = temp % P;
+    return n;
+}
+
+/********************************************************************************************
+* argv[1] needs to be the seed for generate values                                          *
+* argv[2] needs to be the number of test cases                                              *
+* argv[3] needs to be the inferior and superior limit of the range                          *
+* argv[4] needs to be the amount of numbers that needs to be generated for each test case   *
+********************************************************************************************/
 int main(int argc, char* argv[]){
-    srand(atoi(argv[1]));
+    unsigned int seed = gen_rand_seed(atoi(argv[1]));
+    srand(seed);
 
     long long t = atoi(argv[2]);
     long long min, max;
